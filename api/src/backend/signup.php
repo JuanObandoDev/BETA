36f9bea9468c234fb0afd9b7cc56a99a447177c1
Juan Obando <juanobando.developer@gmail.com>
2024-10-10 10:24:00 -0500

    fix: add md5 password encrypted

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -2,8 +2,9 @@
     require "../../config/db_connection.php";
     $email = $_POST['email'];
     $pass = $_POST['pass'];
+    $enc_pass = md5($pass);
 
-    $query = "INSERT INTO users (email, password) VALUES ('$email', '$pass')";
+    $query = "INSERT INTO users (email, password) VALUES ('$email', '$enc_pass')";
     $result = pg_query($conn, $query);
 
     if ($result) {
