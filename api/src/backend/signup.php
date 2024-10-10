ea4583d9f6c452e0431a52cba8b754fa493b0fe6
Juan Obando <juanobando.developer@gmail.com>
2024-10-10 10:57:27 -0500

    refactor: change echo to web browser alert and add redirection to login form

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -2,13 +2,15 @@
     require "../../config/db_connection.php";
     $email = $_POST['email'];
     $pass = $_POST['pass'];
-    $enc_pass = md5($pass);
 
+    $enc_pass = md5($pass);
     $query = "INSERT INTO users (email, password) VALUES ('$email', '$enc_pass')";
     $result = pg_query($conn, $query);
 
     if ($result) {
-        echo "User created successfully";
+        // echo "User created successfully";
+        echo "<script>alert('User created successfully');</script>";
+        header("refresh:0, url=http://127.0.0.1/ProgramacionAvanzada/api/src/login.html");
     } else {
         echo "Error creating user";
     }
