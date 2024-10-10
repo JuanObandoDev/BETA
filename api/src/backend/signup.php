13458c611ec9632c0067e5220a23f6b1e4933101
Juan Obando <juanobando.developer@gmail.com>
2024-10-10 10:58:27 -0500

    feat: add validation if email exists already

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -3,6 +3,17 @@
     $email = $_POST['email'];
     $pass = $_POST['pass'];
 
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_assoc($result);
+    
+    if ($row) {
+        // echo "User already exists";
+        echo "<script>alert('User already exists');</script>";
+        header("refresh:0, url=http://127.0.0.1/ProgramacionAvanzada/api/src/register.html");
+        exit();
+    }
+
     $enc_pass = md5($pass);
     $query = "INSERT INTO users (email, password) VALUES ('$email', '$enc_pass')";
     $result = pg_query($conn, $query);
