73d43f51a5f98da4f2fd2c74411fb35b1964f1f4
Juan Obando <juanobando.developer@gmail.com>
2024-10-16 19:15:45 -0500

    feat: add logic to login an user

--- api/src/backend/signin.php
+++ api/src/backend/signin.php
@@ -1,3 +1,23 @@
 <?php
-    
+    require "../../config/db_connection.php";
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+
+    $enc_pass = md5($pass);
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_assoc($result);
+
+    if ($row) {
+        if ($row['password'] == $enc_pass) {
+            echo "<script>alert('User logged in successfully');</script>";
+            header("refresh:0, url=http://127.0.0.1/ProgramacionAvanzada/api/src/home.php");
+        } else {
+            echo "<script>alert('Incorrect password');</script>";
+            header("refresh:0, url=http://127.0.0.1/ProgramacionAvanzada/api/src/login.html");
+        }
+    } else {
+        echo "<script>alert('User does not exist');</script>";
+        header("refresh:0, url=http://127.0.0.1/ProgramacionAvanzada/api/src/login.html");
+    }
 ?>
