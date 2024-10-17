bcc306a8f42675138faa62e918db82bc60451650
Juan Obando <juanobando.developer@gmail.com>
2024-10-16 19:38:28 -0500

    feat: open a new session if the user logged in successfully

--- api/src/backend/signin.php
+++ api/src/backend/signin.php
@@ -1,15 +1,18 @@
 <?php
     require "../../config/db_connection.php";
+    session_start();
+
     $email = $_POST['email'];
     $pass = $_POST['pass'];
-
     $enc_pass = md5($pass);
+
     $query = "SELECT * FROM users WHERE email = '$email'";
     $result = pg_query($conn, $query);
     $row = pg_fetch_assoc($result);
 
     if ($row) {
         if ($row['password'] == $enc_pass) {
+            $_SESSION['email'] = $email;
             echo "<script>alert('User logged in successfully');</script>";
             header("refresh:0, url=http://127.0.0.1/ProgramacionAvanzada/api/src/home.php");
         } else {
